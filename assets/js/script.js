5338db056768c566078d62bd2c7ad2055de2f177
Daiana de Paula <daianaadepaula1@gmail.com>
2022-04-20 08:03:45 -0300

    notification toast

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//notification toast
+const notificationToast = document.querySelector('[data-toast]');
+const toastCloseBtn = document.querySelector('[data-toast-close]');
+
+toastCloseBtn.addEventListener('click', function () {
+    notificationToast.classList.add('closed');
+});
